cf83cfb72e8def0d72baff03e141ea98c1fcb1b4
quentin <quentin.altieri@gmail.com>
2020-12-28 18:35:20 +0100

    commande de pizza

---
+++ pizzajoint/src/components/Modal.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {motion,AnimatePresence } from 'framer-motion';
+
+const backdrop = {
+
+    visible:{opacity:1},
+    hidden:{opacity:0}
+}
+
+const modal = {
+    hidden:{
+        y:"-100vh",
+        opacity:0
+    },
+    visible:{
+        y:"200px",
+        opacity:1,
+        transition:{delay:0.5}
+    },
+
+}
+
+const Modal = ({showModal,setShowModal}) =>{
+
+    return(
+        <AnimatePresence>
+
+{showModal &&(
+
+<motion.div className ="backdrop"
+variants={backdrop}
+initial="hidden"
+animate="visible"
+exit="hidden"
+>
+
+<motion.div className="modal"
+    variants={modal}
+    >
+
+    <p> Veux-tu faire une nouvelle pizza?</p>
+
+<Link to="/">
+<button> Recommence encore </button>
+
+</Link>
+
+</motion.div>
+</motion.div>
+)}
+</AnimatePresence>
+)
+
+}
+
+export default Modal;
